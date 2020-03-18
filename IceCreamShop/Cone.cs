edef96469d7be29ff90e82543a8f66f369137100
Sally Steuterman <sally.steuterman@gmail.com>
2020-03-18 16:49:50 -0500

    Initial commit

---
+++ IceCreamShop/Cone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IceCreamShop
+{
+    public class Cone : Ingredient
+    {
+        public Cone(string name, double cost, List<string> allergens) : base(name, cost, allergens)
+        {
+        }
+    }
+}
