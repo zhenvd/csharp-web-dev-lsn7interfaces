5a6f348b56fee084e0a2505bb50161296e6c3341
Vincent Zhen <Vincent.D.Zhen@Outlook.com>
2021-07-08 16:48:40 -0400

    added flavor sorter

--- IceCreamShop/Program.cs
+++ IceCreamShop/Program.cs
@@ -11,13 +11,26 @@ namespace IceCreamShop
             List<Flavor> availableFlavors = menu.Flavors;
             List<Cone> availableCones = menu.Cones;
 
-        // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+            // TODO: Use a Comparer class to sort the 'flavors' array alphabetically by the 'name'
+            //  field.
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+            FlavorComparer comparer = new FlavorComparer();
+            Console.WriteLine("Before:\n");
+            foreach(Flavor flavors in availableFlavors)
+            {
+                Console.WriteLine(flavors.Name);
+            }
+            availableFlavors.Sort(comparer);
+            Console.WriteLine("\nAfter:\n");
+            foreach (Flavor flavors in availableFlavors)
+            {
+                Console.WriteLine(flavors.Name);
+            }
+            // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
+            //  field.
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+            // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
         }
     }
 }
